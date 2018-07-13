a77fbb8582266712c9bfb85cc3f2e7987aaccae0
GeunJuLEE <39510739+GeunJuLEE@users.noreply.github.com>
2018-07-13 18:32:47 +0900

    Add files via upload

---
+++ AllCleAPI/AllCleAPI/Models/TodoContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace AllCleAPI.Models
+{
+    public class TodoContext : DbContext
+    {
+        public DbSet<TodoItem> TodoItems { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source = allcle.database.windows.net,1433;Initial Catalog=AllCle;Persist Security Info=False;User ID=allcle;Password=1q2w3e4r!;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30");
+        }
+
+        
+    }
+}
