84d0380b08c33aae5146659c155fc2b99384af10
dlrmswn95 <dlrmswn95@gmail.com>
2018-08-12 17:54:08 +0900

    20180812 17:53

---
+++ WebApplication6/Models/ISubjectRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks; 
+
+
+namespace ApiTest.Models
+{
+    public interface ISubjectRepository
+    {
+        string AddSubject(Subject model); // 입력
+        List<Subject> GetSubjects(); // 출력
+        List<Subject> GetSubjects(string _daylist);
+    }
+}
